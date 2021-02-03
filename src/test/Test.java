f144e000e85f865b9eb5994b9c41f97c2ccd2584
Rion Park <redfilaruntime@gmail.com>
2021-02-03 13:30:46 +0900

    test.html

---
+++ src/test/Test.java
@@ -0,0 +1,15 @@
+package test;
+
+public class Test {
+
+	public static boolean validation(String id, int min, int max, String msg) {
+		System.out.println(id);
+		System.out.println(min);
+		System.out.println(max);
+		System.out.println(msg);
+		return false;
+	}
+	public static void main(String[] args) {
+		//validation("#name");
+	}
+}
